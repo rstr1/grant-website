02d5a771de27988c9b39a5ae096b0edf6707da0d
Grant Dong <129809312+rstr1@users.noreply.github.com>
2025-02-23 12:46:59 +0000

    edited next.config.ts & package.json

--- grant-website/next.config.ts
+++ grant-website/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/grant-website", // this could need to be rstr1.github.io
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
