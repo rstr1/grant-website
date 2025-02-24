b6d0bf4dbd842115988094733d5376d6676459ac
Grant Dong <129809312+rstr1@users.noreply.github.com>
2025-02-24 02:19:44 +0000

    tidied up some unused variables to pass the build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Syne_Mono, Jost } from "next/font/google";
+// import { Geist, Geist_Mono, Syne_Mono, Jost } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
-const syneMono = Syne_Mono({
-  variable: "--font-syne-mono",
-  subsets: ["latin"],
-  weight: "400"
-});
+// const syneMono = Syne_Mono({
+//   variable: "--font-syne-mono",
+//   subsets: ["latin"],
+//   weight: "400"
+// });
 
-const jostFont = Jost({
-  variable: "--font-jost",
-  subsets: ["latin"],
-});
+// const jostFont = Jost({
+//   variable: "--font-jost",
+//   subsets: ["latin"],
+// });
 
 // const libreBaskerville = Libre_Baskerville({
 //   variable: "--font-libre-baskerville",
