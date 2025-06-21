93f398203f61fcb8f807b22cae15bc3fe72740af
Grant Dong <129809312+rstr1@users.noreply.github.com>
2025-06-21 13:53:38 +0100

    hi miche

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,10 @@ export default function Home() {
       
       {/* Testing */}
 
+      <p className="test-white">
+        Herro Miche
+      </p>
+
       <Image
         priority={true}
         src="/photography/DSCF1616.png"
