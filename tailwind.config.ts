14597185ac821e3a781e16d112f0f36001ded325
Grant Dong <129809312+rstr1@users.noreply.github.com>
2025-08-04 14:14:44 +1000

    background colours looked weird on my desktop, changing them to a monotone hue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import {heroui} from '@heroui/theme';
 import type { Config } from "tailwindcss";
 
+
+
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,16 +13,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        foreground: "#000000",      // black
-        dark_background: "#1e1b18", // dark grey
+        // foreground: "#000000",      // black
+        // dark_background: "#1e1d18", // dark grey
         nav_border: "#AB9B94",      // brown/grey
         eggshell: "#F0EAD6",        // eggshell white
         cadmium_orange: "#F28C28",  // cadmium orange
         sky_blue: "#87CEEB",        // sky blue
         other_blue: "#69C3FF",      // other blue
 
-        background: "#1e1d18",      // dark background
-        nav_background: "#1e1d18",  // dark background
+        background: "#202020",      // dark background
+        nav_background: "#202020",  // dark background
         nav_text: "#ffffff",
       },
       // This is where you need to include imported fonts in order to reference via tailwind.css
